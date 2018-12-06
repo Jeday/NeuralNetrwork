d8559743384d80a3757edcabf86932440267a389
unknown <iolanta.paly@gmail.com>
2018-12-06 17:27:45 +0300

    init commit

---
+++ NeuralNetwork1/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NeuralNetwork1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class GenerateImage
+    {
+        public Point start_point;
+        public int margin = 5;
+        public bool[,] img;
+
+        public GenerateImage(Point start)
+        {
+            start_point = start;
+            img = new bool[200, 200];
+
+            for (int i = 0; i < 200; ++i)
+                for (int j = 0; j < 200; ++j)
+                    img[i, j] = false;
+        }
+
+        public GenerateImage() { }
+
+        private void bresenham(int x, int y, int x2, int y2)
+        {
+            int w = x2 - x;
+            int h = y2 - y;
+            int dx1 = 0, dy1 = 0, dx2 = 0, dy2 = 0;
+            if (w < 0) dx1 = -1; else if (w > 0) dx1 = 1;
+            if (h < 0) dy1 = -1; else if (h > 0) dy1 = 1;
+            if (w < 0) dx2 = -1; else if (w > 0) dx2 = 1;
+            int longest = Math.Abs(w);
+            int shortest = Math.Abs(h);
+            if (!(longest > shortest))
+            {
+                longest = Math.Abs(h);
+                shortest = Math.Abs(w);
+                if (h < 0) dy2 = -1; else if (h > 0) dy2 = 1;
+                dx2 = 0;
+            }
+            int numerator = longest >> 1;
+            for (int i = 0; i <= longest; i++)
+            {
+                img[x, y] = true; 
+                numerator += shortest;
+                if (!(numerator < longest))
+                {
+                    numerator -= longest;
+                    x += dx1;
+                    y += dy1;
+                }
+                else
+                {
+                    x += dx2;
+                    y += dy2;
+                }
+            }
+        }
+
+        public void create_triangle()
+        {
+            Random rand = new Random();
+            int right_point = rand.Next(start_point.X, start_point.X + 200 - margin);
+            int left_point = rand.Next(right_point - start_point.X + margin, start_point.X);
+            int up_point = rand.Next(-200 + margin, 200 - margin);
+
+            bresenham(left_point, start_point.Y, right_point, start_point.Y);
+    }
+    }
+}
